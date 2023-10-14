a20a229492aecaa656a8037873695035fc955965
dashubei <dashubei6@gmail.com>
2023-10-14 12:46:15 +0900

    contactのスタイル変更

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES  // この行を追加
-  ? "react_portfolio"            // この行を追加
-  : "./",                     // この行を追加
-  plugins: [react()],
+  base: process.env.GITHUB_PAGES
+    ? "/react_portfolio"
+    : "/",
+  plugins: [react()]
 })
